0f2f7463a400798f588a7bd28687c5f4f0075a4d
tbalcom <trevorbalcom@me.com>
2018-11-13 09:51:50 -0500

    Updates MvvmCross from 6.1.2 to 6.2.2.

--- src/MvxRecyclerViewSelection.Core/ViewModels/Main/MainViewModel.cs
+++ src/MvxRecyclerViewSelection.Core/ViewModels/Main/MainViewModel.cs
@@ -7,9 +7,11 @@ namespace MvxRecyclerViewSelection.Core.ViewModels.Main
 {
     public class MainViewModel : BaseViewModel
     {
-        public MainViewModel()
+        public MainViewModel(IMvxLogProvider mvxLogProvider)
         {
-            PlanetClickCommand = new MvxCommand<PlanetViewModel>((planet) => Log.Info("Clicked planet {0}", planet.Name));
+            _mvxLog = mvxLogProvider.GetLogFor<MainViewModel>();
+
+            PlanetClickCommand = new MvxCommand<PlanetViewModel>((planet) => _mvxLog.Info("Clicked planet {0}", planet.Name));
         }
 
         public override async Task Initialize()
@@ -37,5 +39,7 @@ namespace MvxRecyclerViewSelection.Core.ViewModels.Main
         public MvxCommand<PlanetViewModel> PlanetClickCommand { get; }
 
         private MvxObservableCollection<PlanetViewModel> _planets = new MvxObservableCollection<PlanetViewModel>();
+
+        private readonly IMvxLog _mvxLog;
     }
 }
